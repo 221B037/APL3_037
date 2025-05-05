91775cd0134e0b3ca4f2739ca85eea98e0a825be
221B037 <221B037@JUETGUNA.IN>
2025-05-05 15:53:49 +0530

    RTRT

---
+++ LE_06/Ice_cream.java
@@ -0,0 +1,11 @@
+class Ice_cream extends Decorator{
+    Ice_cream(Offering offering){
+        this.offering=offering;
+    }
+    String getName(){
+        return offering.getName()+" with Ice-cream ";
+    }
+    int getPrice(){
+        return offering.getPrice();
+    }
+}
---
+++ LE_06/Test.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String args[]){
+        Offering offering =new Coffee();
+        offering=new Ice_cream(offering);
+        offering=new Ice_cream(offering);
+        System.out.println(offering.getName()+" "+offering.getPrice());
+    }
+}
